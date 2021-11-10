52e86d439a9c349a6e5b400dbb378ebe3ae74e99
Edouard Bonlieu <edouardb@users.noreply.github.com>
2021-11-10 15:41:32 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello, world!',
+  })
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
